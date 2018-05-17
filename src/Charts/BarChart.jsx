bce98accae1da3718228acb6652a88ba8d2c2530
David Andrus <davida@tune.com>
2018-05-17 13:17:45 -0700

    moving tooltip into it's own component

--- src/Charts/BarChart.jsx
+++ src/Charts/BarChart.jsx
@@ -6,6 +6,7 @@ import { select as d3Select } from 'd3-selection';
 import { scaleLinear } from 'd3-scale';
 import { extent } from 'd3-array';
 import { axisLeft } from 'd3-axis';
+import Tooltipped from './Tooltipped';
 
 function getScaleY(props) {
   const {
@@ -68,6 +69,7 @@ export default class BarChart extends React.Component {
       .attrs((d, i) => getAttrs(d, i, scale, props));
   }
 
+  // @TODO - not currently working
   setSelectedBar = activatedDataIndex => {
     d3Select(this.svg)
       .selectAll('rect.bar')
@@ -79,59 +81,6 @@ export default class BarChart extends React.Component {
         }
       })
   }
-
-  handleMouseMove = (xPos) => {
-    this.lastX = xPos;
-    
-    // this could be cached, but the dom could change around it
-    var bounds = this.svg.getBoundingClientRect();
-    var x = xPos - bounds.left;
-
-    const activatedDataIndex = Math.round(getScaleX(this.props).invert(x));
-    this.tooltipContent.textContent = this.props.data[activatedDataIndex];
-    this.setSelectedBar(activatedDataIndex);
-   
-    d3Select(this.mouseLine)
-      .attrs({
-        stroke: 'gray',
-        'stroke-width': 1,
-        x1: x,
-        x2: x,
-        y1: 0,
-        y2: this.props.height
-      });
-
-    const xTrans = x < this.props.width / 2 
-      ? x + 20
-      : x - 200 - 20;
-
-    d3Select(this.tooltip)
-      .attrs({
-        transform: `translate(${xTrans})`,
-      })
-  }
-
-  handleMouseLeave = () => {
-    d3Select(this.tooltip)
-      .attrs({
-        transform: `translate(-10000)`,
-      })
-
-    d3Select(this.mouseLine)
-      .attrs({
-        transform: `translate(-10000)`,
-      });
-      
-      this.setSelectedBar(-1);
-  }
-
-  performantMouseMove = e => {
-    const { clientX } = e;
-    if (clientX !== this.lastX) {
-      window.requestAnimationFrame(() => this.handleMouseMove(clientX));
-    }
-  }
-
 
   renderAxis() {
     var node  = this.axis;
@@ -148,17 +97,16 @@ export default class BarChart extends React.Component {
     } = this.props;
 
     const scale = getScaleY(this.props);
-    
     return (
       <svg
         ref={node => { this.svg = node; }}
         height={height}
         width={width}
       >
-        <line ref={node => this.mouseLine = node} />
-        <g transform="translate(50)"
-          onMouseMove={this.performantMouseMove}
-          onMouseLeave={this.handleMouseLeave}
+        <Tooltipped
+          data={data}
+          height={height} 
+          width={width}
         >
           {data.map((point, index) => (
             <rect
@@ -167,18 +115,7 @@ export default class BarChart extends React.Component {
               key={index}
             />
           ))}
-        </g>
-        <g ref={node => { this.axis = node }} transform="translate(50)" />
-        <g ref={node => { this.tooltip = node; }} transform="translate(-10000)">
-          <rect 
-            fill='red'
-            height={200}
-            width={200}
-          />
-          <foreignObject width="100" height="50">
-            <p ref={node => this.tooltipContent = node}></p>
-          </foreignObject>
-        </g>
+        </Tooltipped>
       </svg>
     );
   }
---
+++ src/Charts/Tooltipped.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+
+import { select as d3Select } from 'd3-selection';
+import { scaleLinear } from 'd3-scale';
+
+function getScaleX(props) {
+  const {
+    data,
+    width,
+  } = props;
+
+  return scaleLinear()
+    .domain([0, data.length])
+    .range([0, width]);
+}
+
+export default class Tooltipped extends React.Component {
+  
+  handleMouseMove = (xPos) => {
+    this.lastX = xPos;
+    
+    // this could be cached, but the dom could change around it
+    var bounds = this.mouseLayer.getBoundingClientRect();
+    var x = xPos - bounds.left;
+
+    const activatedDataIndex = Math.round(getScaleX(this.props).invert(x));
+    this.tooltipContent.textContent = this.props.data[activatedDataIndex];
+    // this.setSelectedBar(activatedDataIndex);
+   
+    d3Select(this.mouseLine)
+      .attrs({
+        stroke: 'gray',
+        'stroke-width': 1,
+        transform: 'translate(0)',
+        x1: x,
+        x2: x,
+        y1: 0,
+        y2: this.props.height
+      });
+
+    const xTrans = x < this.props.width / 2 
+      ? x + 20
+      : x - 200 - 20;
+
+    d3Select(this.tooltip)
+      .attrs({
+        transform: `translate(${xTrans})`,
+      })
+  }
+
+  handleMouseLeave = () => {
+    d3Select(this.tooltip)
+      .attrs({
+        transform: `translate(-10000)`,
+      })
+
+    d3Select(this.mouseLine)
+      .attrs({
+        transform: `translate(-10000)`,
+      });
+      
+      // this.setSelectedBar(-1);
+  }
+
+  performantMouseMove = e => {
+    const { clientX } = e;
+    if (clientX !== this.lastX) {
+      window.requestAnimationFrame(() => this.handleMouseMove(clientX));
+    }
+  }
+
+  render() {
+    const { children, height, width } = this.props;
+    return (
+      <g>
+        <g>{this.props.children}</g>
+        <line ref={node => this.mouseLine = node} />
+        <g ref={node => { this.tooltip = node; }} transform="translate(-10000)">
+         <rect 
+           fill='red'
+           height={200}
+           width={200}
+         />
+         <foreignObject width="100" height="50">
+           <p ref={node => this.tooltipContent = node}></p>
+         </foreignObject>
+       </g>
+        <g
+          ref={node => {this.mouseLayer = node; }}
+          onMouseMove={this.performantMouseMove}
+          onMouseLeave={this.handleMouseLeave}
+        >
+          <rect
+            fill='red'
+            fill-opacity='0'
+            x="0"
+            y="0" 
+            height={height}
+            width={width}
+          />
+        </g>
+      </g>
+    );
+  }
+}
