062443ab26f08aaf7ee24ea1f4d16d548147a98f
David Andrus <davida@tune.com>
2018-05-23 15:51:57 -0700

    getting to toolip selections hovering as expected

---
+++ src/Charts/LineChartTooltipSelection.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Group } from '@vx/group';
+
+export default function LineChartTooltipSelection(props) {
+  console.log({ props })
+  return <Group><rect /></Group>;
+}
