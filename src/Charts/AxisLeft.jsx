7573dc1bd4d803aa894d76f0840c532fe68b2caa
David Andrus <davida@tune.com>
2018-05-25 11:21:09 -0700

    getting basic vertical axis label going

--- src/Charts/AxisLeft.jsx
+++ src/Charts/AxisLeft.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { findDOMNode } from 'react-dom';
 import { AxisLeft as VXAxisLeft} from '@vx/axis';
+import { Line } from '@vx/shape';
+import { Group} from '@vx/group';
+import { Text } from '@vx/text';
 
 import {
   axisStroke,
+  labelLineHeight,
+  labelLineSpacing,
   yAxisWidth,
 } from './constants';
 
@@ -16,16 +22,84 @@ const tickLabelPropsLeft = {
   fill: 'red'
 };
 
-export default function AxisLeft({ label, scale }) {
-  return (
-    <VXAxisLeft
-      scale={scale}
-      label={label}
-      left={yAxisWidth}
-      numTicks={5}
-      stroke={axisStroke}
-      tickLabelProps={() => tickLabelPropsLeft}
-      tickStroke={axisStroke}
-    />
-  );
+const getTransform = labelY => `translate(-45, ${labelY}) rotate(-90)`;
+
+export default class AxisLeft extends React.Component {
+
+  state = { labelWidth: 0 }
+
+  componentDidMount() {
+    this.label = findDOMNode(this._label);
+    this.line = findDOMNode(this._line);
+    this.group = findDOMNode(this._group);
+
+    this.updatePositions();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.text !== nextProps.text;
+  }
+
+  componentWillUpdate() {
+    this.updatePositions();
+  }
+
+  getLine = (line) => {
+    this._line = line;
+  }
+
+  getLabel = (label) => {
+    this._label = label;
+  }
+
+  getGroup = (group) => {
+    this._group = group;
+  }
+
+  updatePositions = () => {
+    const { scale } = this.props;
+    const labelWidth = this.label.getBBox().width;
+    const height = Math.max(...scale.range());
+    const labelY = (height / 2) + ((70 + labelLineHeight + labelLineSpacing) / 2);
+    this.line.setAttribute('x1', labelWidth + labelLineSpacing);
+    this.line.setAttribute('x2', labelWidth + labelLineSpacing + labelLineHeight);
+    this.group.setAttribute('transform', getTransform(labelY));
+  }
+
+  render() {
+    const { label, scale } = this.props;
+
+    return (
+      <Group left={yAxisWidth}>
+        <g ref={this.getGroup} transform={getTransform(0)}>
+          <text
+            fontSize='12'
+            dy={3}
+            x={0}
+            y={0}
+            ref={this.getLabel}
+            verticalAnchor="end"
+          >
+            {label}
+          </text>
+          <line
+            ref={this.getLine}
+            x1={0}
+            y1={0}
+            x2={0}
+            y2={0}
+            stroke="black"
+          />
+
+        </g>
+        <VXAxisLeft
+          scale={scale}
+          numTicks={5}
+          stroke={axisStroke}
+          tickLabelProps={() => tickLabelPropsLeft}
+          tickStroke={axisStroke}
+        />
+      </Group>
+    );
+  }
 }
--- src/Charts/constants.js
+++ src/Charts/constants.js
@@ -1,6 +1,8 @@
-export const yAxisWidth = 50;
+export const yAxisWidth = 70;
 export const xAxisHeight = 50;
 export const topPadding = 20;
 export const rightPadding = 20;
+export const labelLineHeight = 36;
+export const labelLineSpacing = 10;
 export const axisStroke = 'gray';
 
